4cf7122519aac851591bd52dc6f445d8138e5c07
Dinara <auliasafinadinara@gmail.com>
2026-04-27 07:48:31 +0700

    fix peminjaman table migration

--- database/migrations/2026_02_21_020528_create_peminjamen_table.php
+++ database/migrations/2026_02_21_020528_create_peminjamen_table.php
@@ -13,26 +13,21 @@ return new class extends Migration
     {
         Schema::create('peminjamans', function (Blueprint $table) {
             $table->string('id_peminjaman', 100)->primary();
+            $table->string('kategori_instansi', 100)->nullable();
             $table->string('ruangan_id', 100)->nullable();
-            $table->string('nama_kegiatan', 100)->nullable();
-            $table->string('latar_belakang', 100)->nullable();
-            $table->string('tujuan_kegiatan', 100)->nullable();
-            $table->string('sasaran_peserta', 100)->nullable();
-            $table->integer('jumlah_peserta')->nullable();
-            $table->string('narasumber', 100)->nullable();
-            $table->string('pj_kegiatan', 100)->nullable();
             $table->string('instansi', 100)->nullable();
-            $table->string('alamat_instansi', 100)->nullable();
-            $table->string('wilayah', 100)->nullable();
-            $table->integer('no_hp_pj')->nullable();
-            $table->string('fasilitas_tambahan', 100)->nullable();
+            $table->string('alamat_instansi', 255)->nullable();
+            $table->string('nama_kegiatan', 100)->nullable();
+            $table->integer('jumlah_peserta')->nullable();
+            $table->string('pj_kegiatan', 100)->nullable();
+            $table->string('no_hp_pj', 50)->nullable();
             $table->date('tgl_penggunaan')->nullable();
-            $table->timestamp('jam_mulai')->nullable();
-            $table->timestamp('jam_berakhir')->nullable();
-            $table->string('berkas_ktp', 100)->nullable();
-            $table->string('berkas_surat', 100)->nullable();
-            $table->string('berkas_poster', 100)->nullable();
-            $table->boolean('status')->default(false);
+            $table->date('tgl_berakhir')->nullable();
+            $table->time('jam_mulai')->nullable();
+            $table->time('jam_berakhir')->nullable();
+            $table->string('berkas_surat', 255)->nullable();
+            $table->boolean('bersedia_ubah_jadwal')->default(false);
+            $table->string('status')->default('pending');
             $table->timestamps();
         });
     }
